afd4d426fab92d09160a3b408193cdd515293036
mishu <mishu@xtelecom.ro>
2017-10-05 15:05:33 +0300

    refactor routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,21 +1,18 @@
 <?php
 
-Route::middleware(['auth:api', 'api', 'core'])
-    ->prefix('api')
+Route::middleware(['web', 'auth', 'core'])
+    ->prefix('api/system')->as('system.')
     ->namespace('LaravelEnso\MenuManager\app\Http\Controllers')
     ->group(function () {
-        Route::prefix('system')->as('system.')
+        Route::prefix('menus')->as('menus.')
             ->group(function () {
-                Route::prefix('menus')->as('menus.')
-                    ->group(function () {
-                        Route::get('initTable', 'MenuTableController@initTable')
-                            ->name('initTable');
-                        Route::get('getTableData', 'MenuTableController@getTableData')
-                            ->name('getTableData');
-                        Route::get('exportExcel', 'MenuTableController@exportExcel')
-                            ->name('exportExcel');
-                    });
-
-                Route::resource('menus', 'MenuController', ['except' => ['show']]);
+                Route::get('initTable', 'MenuTableController@initTable')
+                    ->name('initTable');
+                Route::get('getTableData', 'MenuTableController@getTableData')
+                    ->name('getTableData');
+                Route::get('exportExcel', 'MenuTableController@exportExcel')
+                    ->name('exportExcel');
             });
+
+        Route::resource('menus', 'MenuController', ['except' => ['show']]);
     });
