7d3e09f60cd56b9319fdd7c74f79f42ba73c49d3
mishu <mishu@xtelecom.ro>
2018-01-19 08:28:45 +0200

    removes explicit restrictions from migrations

--- src/database/migrations/2017_01_01_102000_create_menus_table.php
+++ src/database/migrations/2017_01_01_102000_create_menus_table.php
@@ -10,8 +10,7 @@ class CreateMenusTable extends Migration
         Schema::create('menus', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('parent_id')->unsigned()->index()->nullable();
-            $table->foreign('parent_id')->references('id')->on('menus')
-                ->onUpdate('restrict')->onDelete('restrict');
+            $table->foreign('parent_id')->references('id')->on('menus');
             $table->string('name');
             $table->string('icon');
             $table->integer('order');
