040796997fd67382ad5eef05228ffc95085bdf2d
mishu <mishu@xtelecom.ro>
2018-04-03 18:45:14 +0300

    updates for the new datatable use

--- src/app/Http/Controllers/MenuTableController.php
+++ src/app/Http/Controllers/MenuTableController.php
@@ -3,21 +3,13 @@
 namespace LaravelEnso\MenuManager\app\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use LaravelEnso\MenuManager\app\Models\Menu;
 use LaravelEnso\VueDatatable\app\Traits\Excel;
 use LaravelEnso\VueDatatable\app\Traits\Datatable;
+use LaravelEnso\MenuManager\app\Tables\Builders\MenuTable;
 
 class MenuTableController extends Controller
 {
     use Datatable, Excel;
 
-    private const Template = __DIR__.'/../../Tables/menus.json';
-
-    public function query()
-    {
-        return Menu::select(\DB::raw(
-            'menus.id as "dtRowId", menus.name, menus.icon, menus.has_children, menus.order,
-            parent_menus.name as parent, menus.link, menus.created_at, menus.updated_at'
-        ))->leftJoin('menus as parent_menus', 'menus.parent_id', '=', 'parent_menus.id');
-    }
+    protected $tableClass = MenuTable::class;
 }
---
+++ src/app/Tables/Builders/MenuTable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LaravelEnso\MenuManager\app\Tables\Builders;
+
+use LaravelEnso\MenuManager\app\Models\Menu;
+use LaravelEnso\VueDatatable\app\Classes\Table;
+
+class MenuTable extends Table
+{
+    protected $templatePath = __DIR__.'/../Templates/menus.json';
+
+    public function query()
+    {
+        return Menu::select(\DB::raw(
+            'menus.id as "dtRowId", menus.name, menus.icon, menus.has_children, menus.order,
+            parent_menus.name as parent, menus.link, menus.created_at, menus.updated_at'
+        ))->leftJoin('menus as parent_menus', 'menus.parent_id', '=', 'parent_menus.id');
+    }
+}
