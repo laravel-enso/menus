cc5453db5d4485b58711fc7af48c1d88b593a406
mishu <mishu@xtelecom.ro>
2018-04-10 20:44:26 +0300

    closes laravel-enso/enso#92

--- src/app/Requests/ValidateMenuRequest.php
+++ src/app/Requests/ValidateMenuRequest.php
@@ -19,4 +19,19 @@ class ValidateMenuRequest extends FormRequest
             'has_children' => 'required',
         ];
     }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            if (request()->get('has_children') && request()->filled('link')) {
+                $validator->errors()->add('has_children', 'The menu must not to be a parent if the link is not null');
+                $validator->errors()->add('link', 'The link has to be null if the menu is a parent');
+            }
+
+            if (!request()->get('has_children') && !request()->filled('link')) {
+                $validator->errors()->add('has_children', 'The menu must be a parent if the link is null');
+                $validator->errors()->add('link', 'The link cannot be null if the menu is not a parent');
+            }
+        });
+    }
 }
