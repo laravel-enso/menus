eadc5cb8aab615ccfba86b7afee41b7330717381
Adrian Ocneanu <aocneanu@gmail.com>
2018-09-16 14:59:52 +0000

    Apply fixes from StyleCI

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -36,7 +36,6 @@ class AppServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/database/factories' => database_path('factories'),
         ], 'enso-factories');
-
     }
 
     public function register()
