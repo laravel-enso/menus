45dda9484a56ad5c4694af519cf3a4ad4212c565
mishu <mishu@xtelecom.ro>
2019-03-08 15:13:30 +0200

    moves FE assets to npm

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -15,7 +15,6 @@ class AppServiceProvider extends ServiceProvider
     private function loadDependencies()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/api.php');
-
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         return $this;
@@ -23,14 +22,6 @@ class AppServiceProvider extends ServiceProvider
 
     private function publishDependencies()
     {
-        $this->publishes([
-            __DIR__.'/resources/js' => resource_path('js'),
-        ], 'menus-assets');
-
-        $this->publishes([
-            __DIR__.'/resources/js' => resource_path('js'),
-        ], 'enso-assets');
-
         $this->publishes([
             __DIR__.'/database/factories' => database_path('factories'),
         ], 'menus-factory');
