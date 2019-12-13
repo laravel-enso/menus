7f7ad13b560c1363d0b2b8c610a83439c06169d4
Mihai <mihai.ocneanu@gmail.com>
2019-12-13 17:53:23 +0200

    split routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,14 +5,5 @@ Route::middleware(['web', 'auth', 'core'])
     ->as('system.menus.')
     ->namespace('LaravelEnso\Menus\app\Http\Controllers')
     ->group(function () {
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{menu}/edit', 'Edit')->name('edit');
-        Route::patch('{menu}', 'Update')->name('update');
-        Route::delete('{menu}', 'Destroy')->name('destroy');
-        Route::put('organize', 'Organize')->name('organize');
-
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
-        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+        require 'app/menus.php';
     });
