293e339b2e636ca7eceed2371f4bcbd44c9b036b
robbykrlos <robby_krlos@yahoo.com>
2020-01-22 18:43:45 +0200

    Related to Spa and AppState flexibility #272 - made TreeBuilder private methods protected for flexibility and customization purposes.

--- src/app/Services/TreeBuilder.php
+++ src/app/Services/TreeBuilder.php
@@ -21,7 +21,7 @@ class TreeBuilder
             ->build();
     }
 
-    private function build(?int $parentId = null): Collection
+    protected function build(?int $parentId = null): Collection
     {
         return $this->menus
             ->filter(fn ($menu) => $menu->parent_id === $parentId)
@@ -29,7 +29,7 @@ class TreeBuilder
         ), new Collection());
     }
 
-    private function withChildren(Menu $menu): Menu
+    protected function withChildren(Menu $menu): Menu
     {
         $menu->children = $menu->has_children
             ? $this->build($menu->id)
@@ -44,7 +44,7 @@ class TreeBuilder
         return $menu;
     }
 
-    private function permissions(): self
+    protected function permissions(): self
     {
         $this->permissions = Auth::user()->role
             ->permissions()
@@ -54,7 +54,7 @@ class TreeBuilder
         return $this;
     }
 
-    private function menus(): self
+    protected function menus(): self
     {
         $this->menus = Menu::with('permission:id,name')
             ->orderBy('order_index')
@@ -63,21 +63,21 @@ class TreeBuilder
         return $this;
     }
 
-    private function filter(): self
+    protected function filter(): self
     {
         $this->menus = $this->menus->filter(fn ($menu) => $this->allowed($menu));
 
         return $this;
     }
 
-    private function map(): self
+    protected function map(): self
     {
         $this->menus = $this->menus->map(fn ($menu) => $this->computeIcon($menu));
 
         return $this;
     }
 
-    private function computeIcon(Menu $menu): Menu
+    protected function computeIcon(Menu $menu): Menu
     {
         if (Str::contains($menu->icon, ' ')) {
             $menu->icon = explode(' ', $menu->icon);
@@ -86,13 +86,13 @@ class TreeBuilder
         return $menu;
     }
 
-    private function allowed($menu): bool
+    protected function allowed($menu): bool
     {
         return $this->permissions->pluck('id')->contains($menu->permission_id)
             || $menu->has_children && $this->someChildrenAllowed($menu);
     }
 
-    private function someChildrenAllowed($parent): bool
+    protected function someChildrenAllowed($parent): bool
     {
         return $this->menus->some(
             fn ($child) => $child->parent_id === $parent->id && $this->allowed($child)
