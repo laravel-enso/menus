75cc5b18e93991ec8243d4138209f47df8811390
mishu <mishu@xtelecom.ro>
2020-03-10 12:00:44 +0200

    updates core req to 4.8

--- src/app/Tables/Builders/MenuTable.php
+++ src/app/Tables/Builders/MenuTable.php
@@ -4,11 +4,12 @@ namespace LaravelEnso\Menus\App\Tables\Builders;
 
 use Illuminate\Database\Eloquent\Builder;
 use LaravelEnso\Menus\App\Models\Menu;
+use LaravelEnso\Tables\App\Contracts\CustomCountCacheKey;
 use LaravelEnso\Tables\App\Contracts\Table;
 
 class MenuTable implements Table
 {
-    protected const TemplatePath = __DIR__.'/../Templates/menus.json';
+    protected const TemplatePath = __DIR__ . '/../Templates/menus.json';
 
     public function query(): Builder
     {
@@ -16,7 +17,7 @@ class MenuTable implements Table
             menus.id, menus.name, menus.icon, menus.has_children, menus.order_index,
             parent_menus.name as parent, permissions.name as route, menus.created_at
         ')->leftJoin('permissions', 'menus.permission_id', '=', 'permissions.id')
-        ->leftJoin('menus as parent_menus', 'menus.parent_id', '=', 'parent_menus.id');
+            ->leftJoin('menus as parent_menus', 'menus.parent_id', '=', 'parent_menus.id');
     }
 
     public function templatePath(): string
