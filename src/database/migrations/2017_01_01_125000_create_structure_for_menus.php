934e325571fd56ef8ead9ffc343f3d767abbf3be
mishu <mishu@xtelecom.ro>
2020-03-14 18:32:50 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_125000_create_structure_for_menus.php
+++ src/database/migrations/2017_01_01_125000_create_structure_for_menus.php
@@ -1,21 +1,20 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForMenus extends Migration
 {
     protected $permissions = [
-        ['name' => 'system.menus.index', 'description' => 'Menus index', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.menus.tableData', 'description' => 'Get table data for menus', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.menus.exportExcel', 'description' => 'Export excel for menus', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.menus.initTable', 'description' => 'Init table for menus menu', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.menus.create', 'description' => 'Create menu', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.menus.edit', 'description' => 'Edit menu', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.menus.store', 'description' => 'Store newly created menu', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.menus.update', 'description' => 'Update edited menu', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.menus.destroy', 'description' => 'Delete menu', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.menus.organize', 'description' => 'Organize menus', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'system.menus.index', 'description' => 'Menus index', 'is_default' => false],
+        ['name' => 'system.menus.tableData', 'description' => 'Get table data for menus', 'is_default' => false],
+        ['name' => 'system.menus.exportExcel', 'description' => 'Export excel for menus', 'is_default' => false],
+        ['name' => 'system.menus.initTable', 'description' => 'Init table for menus menu', 'is_default' => false],
+        ['name' => 'system.menus.create', 'description' => 'Create menu', 'is_default' => false],
+        ['name' => 'system.menus.edit', 'description' => 'Edit menu', 'is_default' => false],
+        ['name' => 'system.menus.store', 'description' => 'Store newly created menu', 'is_default' => false],
+        ['name' => 'system.menus.update', 'description' => 'Update edited menu', 'is_default' => false],
+        ['name' => 'system.menus.destroy', 'description' => 'Delete menu', 'is_default' => false],
+        ['name' => 'system.menus.organize', 'description' => 'Organize menus', 'is_default' => false],
     ];
 
     protected $menu = [
