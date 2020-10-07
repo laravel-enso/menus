6d08fa99886cbf15d17c8d196c45408cd769583c
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:26:15 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Menus\Http\Controllers\Create;
+use LaravelEnso\Menus\Http\Controllers\Destroy;
+use LaravelEnso\Menus\Http\Controllers\Edit;
+use LaravelEnso\Menus\Http\Controllers\ExportExcel;
+use LaravelEnso\Menus\Http\Controllers\InitTable;
+use LaravelEnso\Menus\Http\Controllers\Organize;
+use LaravelEnso\Menus\Http\Controllers\Store;
+use LaravelEnso\Menus\Http\Controllers\TableData;
+use LaravelEnso\Menus\Http\Controllers\Update;
 
 Route::middleware(['api', 'auth', 'core'])
     ->prefix('api/system/menus')
     ->as('system.menus.')
-    ->namespace('LaravelEnso\Menus\Http\Controllers')
     ->group(function () {
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{menu}/edit', 'Edit')->name('edit');
-        Route::patch('{menu}', 'Update')->name('update');
-        Route::delete('{menu}', 'Destroy')->name('destroy');
-        Route::put('organize', 'Organize')->name('organize');
+        Route::get('create', Create::class)->name('create');
+        Route::post('', Store::class)->name('store');
+        Route::get('{menu}/edit', Edit::class)->name('edit');
+        Route::patch('{menu}', Update::class)->name('update');
+        Route::delete('{menu}', Destroy::class)->name('destroy');
+        Route::put('organize', Organize::class)->name('organize');
 
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
-        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+        Route::get('initTable', InitTable::class)->name('initTable');
+        Route::get('tableData', TableData::class)->name('tableData');
+        Route::get('exportExcel', ExportExcel::class)->name('exportExcel');
     });
